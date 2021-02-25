0b941f938e501f23e86d9bb8d21ea0b1d3f711fa
Gustavo <gustavo-dev@hotmail.com>
2021-02-25 10:19:29 -0300

    Componente: button > Completei Finalizado

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-    const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
+    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
 
     return (
         <div className={ styles.challengeBoxContainer }>
@@ -28,6 +29,7 @@ export function ChallengeBox() {
                         <button 
                             type="button"
                             className={styles.challengeSucceededButton}
+                            onClick={completeChallenge}
                         >
                             Completei
                         </button>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile(){
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={ styles.profileContainer }>
             <img src="https://pbs.twimg.com/profile_images/1358229460684791808/pGWnIySr_400x400.jpg" alt="gustavo Venâncio"/>
@@ -8,7 +12,7 @@ export function Profile(){
                 <strong>Gustavo Venâncio</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
